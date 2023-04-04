69274a349c8d72e8868211953db16ea98b857ab6
Finkle <daniel.finkle1@gmail.com>
2023-04-04 15:33:55 -0700

    practice using async/promises

---
+++ node-fundamentals/async/index.js
@@ -0,0 +1,76 @@
+baseURL = "http://numbersapi.com";
+let favNumber = 5;
+
+async function numData() {
+  let data = await axios.get(`${baseURL}/${favNumber}?json`);
+  console.log(data);
+}
+
+numData();
+
+let favNumbers = [7, 11, 22];
+
+async function favorites() {
+  let data = await axios.get(`${baseURL}/${favNumbers}?json`);
+  console.log(data);
+}
+favorites();
+
+async function facts() {
+  let facts = await Promise.all(
+    Array.from({ length: 4 }, () => axios.get(`${baseURL}/${favNumber}?json`))
+  );
+  console.log(facts);
+  facts.forEach(({ data }) => {
+    $("body").append(`<p>${data.text}</p>`);
+  });
+}
+
+facts();
+
+$(function () {
+  let baseURL = "https://deckofcardsapi.com/api/deck";
+
+  // 1.
+  async function part1() {
+    let data = await $.getJSON(`${baseURL}/new/draw/`);
+    let { suit, value } = data.cards[0];
+    console.log(`${value.toLowerCase()} of ${suit.toLowerCase()}`);
+  }
+
+  // 2.
+  async function part2() {
+    let firstCardData = await $.getJSON(`${baseURL}/new/draw/`);
+    let deckId = firstCardData.deck_id;
+    let secondCardData = await $.getJSON(`${baseURL}/${deckId}/draw/`);
+    [firstCardData, secondCardData].forEach((card) => {
+      let { suit, value } = card.cards[0];
+      console.log(`${value.toLowerCase()} of ${suit.toLowerCase()}`);
+    });
+  }
+
+  // 3.
+  async function setup() {
+    let $btn = $("button");
+    let $cardArea = $("#card-area");
+
+    let deckData = await $.getJSON(`${baseURL}/new/shuffle/`);
+    $btn.show().on("click", async function () {
+      let cardData = await $.getJSON(`${baseURL}/${deckData.deck_id}/draw/`);
+      let cardSrc = cardData.cards[0].image;
+      let angle = Math.random() * 90 - 45;
+      let randomX = Math.random() * 40 - 20;
+      let randomY = Math.random() * 40 - 20;
+      $cardArea.append(
+        $("<img>", {
+          src: cardSrc,
+          css: {
+            transform: `translate(${randomX}px, ${randomY}px) rotate(${angle}deg)`,
+          },
+        })
+      );
+      if (cardData.remaining === 0) $btn.remove();
+    });
+  }
+  setup();
+});
