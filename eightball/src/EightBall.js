037e9705a2c1e2ee4e91486aead74510a4fcdaa0
Finkle <daniel.finkle1@gmail.com>
2023-04-26 11:31:41 -0700

    EightBall react application remainder

---
+++ eightball/src/EightBall.js
@@ -0,0 +1,35 @@
+import React, { useState } from "react";
+import "./EightBall.css";
+
+function choice(arr) {
+  const randomIdx = Math.floor(Math.random() * arr.length);
+  return arr[randomIdx];
+}
+const Eightball = (props) => {
+  const [msg, setMsg] = useState("Think of a Question");
+  const [color, setColor] = useState("black");
+  const restart = () => {
+    setMsg("Think of a Question");
+    setColor("black");
+  };
+  const handleClick = () => {
+    const { msg, color } = choice(props.answers);
+    setMsg(msg);
+    setColor(color);
+  };
+
+  return (
+    <>
+      <div
+        onClick={handleClick}
+        className="EightBall"
+        style={{ backgroundColor: color }}
+      >
+        {msg}
+      </div>
+      <button onClick={restart}>Restart Game</button>
+    </>
+  );
+};
+
+export default Eightball;
